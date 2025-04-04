d4ec007ffbab0a2f08e0db3432815c2d40ff2800
Katy248 <petrovanton247@gmail.com>
2025-04-04 12:01:36 +0300

    Added ssh configuration

---
+++ management/ssh/ssh.go
@@ -0,0 +1,48 @@
+package ssh
+
+import (
+	"ras/management/systemctl"
+	"ras/utils"
+	"strings"
+)
+
+type SshStatus struct {
+	Enabled bool `json:"enabled"`
+}
+
+const SshService = "sshd"
+
+var defaultStatus = SshStatus{
+	Enabled: false,
+}
+
+func Status() SshStatus {
+	sshEnabled := systemctl.IsActive("ssh")
+
+	return SshStatus{
+		Enabled: sshEnabled,
+	}
+}
+
+func Enable() error {
+	return systemctl.Enable(SshService)
+}
+func Disable() error {
+	return systemctl.Disable(SshService)
+}
+
+func GetKeys() ([]string, error) {
+	output, err := utils.Execute("ssh-keyscan", "localhost")
+	if err != nil {
+		return nil, err
+	}
+	strs := strings.Split(string(output), "\n")
+	keys := make([]string, len(strs))
+	for _, s := range strs {
+		if s[0] != '#' {
+			keys = append(keys, s)
+		}
+	}
+
+	return keys, nil
+}
