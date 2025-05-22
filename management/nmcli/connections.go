3ea7fba08fc276875ebfd5897e418ff3b24e01f9
Katy248 <petrovanton247@gmail.com>
2025-05-22 11:33:58 +0300

    Add more logging

--- management/nmcli/connections.go
+++ management/nmcli/connections.go
@@ -3,7 +3,6 @@ package nmcli
 import (
 	"fmt"
 	"ras/utils"
-	"strconv"
 	"strings"
 
 	"github.com/charmbracelet/log"
@@ -24,6 +23,7 @@ type ConnectionType string
 
 const (
 	ConnectionTypeWIFI     ConnectionType = "wifi"
+	ConnectionTypeWireless ConnectionType = "802-11-wireless" // Access point connection
 	ConnectionTypeEthernet ConnectionType = "ethernet"
 )
 
@@ -108,34 +108,6 @@ func CreateWirelessConnection(
 const (
 	OptionKeyIP4Method = "ipv4.method"
 )
-const (
-	OptionKeyWirelessSSID     = "802-11-wireless.ssid"
-	OptionKeyWirelessHidden   = "802-11-wireless.hidden"
-	OptionKeyWirelessChanel   = "802-11-wireless.chanel"
-	OptionKeyWirelessPassword = "wifi-sec.psk"
-	OptionKeyWirelessMode     = "802-11-wireless.mode"
-	OptionKeyWirelessKeyMgmt  = "wifi-sec.key-mgmt" //Probably security mode
-)
-
-type WirelessMode string
-
-const (
-	WirelessModeAccessPoint WirelessMode = "ap"
-)
-
-func (c *WirelessConnection) SetMode(mode WirelessMode) error {
-	return c.setOption(OptionKeyWirelessMode, string(mode))
-}
-
-type WirelessBand = string
-
-const (
-	WirelessBandDefault WirelessBand = "bg"
-)
-
-func (c *WirelessConnection) SetBand(band WirelessBand) error {
-	return c.setOption(OptionKeyWirelessMode, string(band))
-}
 
 type IP4Method = string
 
@@ -153,56 +125,6 @@ func (c *Connection) Up() error {
 func (c *Connection) Down() error {
 	return utils.ExecuteErr("nmcli", "connection", "up", c.Name)
 }
-func (c *WirelessConnection) GetSSID() string {
-	return c.getOption(OptionKeyWirelessSSID)
-}
-func (c *WirelessConnection) SetSSID(ssid string) error {
-	err := c.setOption(OptionKeyWirelessSSID, ssid)
-	if err == nil {
-		c.ensureOptionsParsed()
-	}
-	return err
-}
-func (c *WirelessConnection) GetChanel() int {
-	opt := c.getOption(OptionKeyWirelessChanel)
-	value, err := strconv.Atoi(opt)
-	if err != nil {
-		return 0
-	}
-	return value
-}
-func (c *WirelessConnection) SetChannel(chanel int) error {
-	return c.setOption(OptionKeyWirelessChanel, strconv.Itoa(chanel))
-}
-func (c *WirelessConnection) GetPassword() string {
-	return c.getOption(OptionKeyWirelessPassword)
-}
-func (c *WirelessConnection) SetPassword(password string) error {
-	return c.setOption(OptionKeyWirelessPassword, password)
-}
-
-const (
-	WirelessHiddenValue    = "no"
-	WirelessNotHiddenValue = "yes"
-)
-
-func (c *WirelessConnection) IsHidden() bool {
-	return c.getOption(OptionKeyWirelessHidden) == WirelessHiddenValue
-}
-func (c *WirelessConnection) SetHidden(hide bool) error {
-	var value string
-	if hide {
-		value = WirelessHiddenValue
-	} else {
-		value = WirelessNotHiddenValue
-	}
-	err := c.setOption(OptionKeyWirelessHidden, value)
-	if err == nil {
-		c.ensureOptionsParsed()
-	}
-	return err
-}
-
 func (c *Connection) getOption(optionName string) string {
 	c.ensureOptionsParsed()
 	return c.options[optionName]
@@ -222,6 +144,7 @@ func (c *Connection) ensureOptionsParsed() error {
 }
 
 func (c *Connection) setOption(optionName, optionValue string) error {
+	log.Debugf("Setting option %s to '%s', current value is '%s'", optionName, optionValue, c.options[optionName])
 	return utils.ExecuteErr("nmcli", "connection", "modify", c.Name, optionName, optionValue)
 }
 
